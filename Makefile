ic-designer/make-bowerbird-help

initial commit of files in new repo

Makefile
@@ -1,0 +1,22 @@
+# Constants
+.DEFAULT_GOAL := help
+
+NAME := boxerbird-help
+VERSION := $(shell git describe --always --dirty --broken 2> /dev/null)
+WORKDIR_ROOT := $(CURDIR)/.make
+
+#Targets
+.PHONY: check
+## Runs the repository tests
+check: private_test
+
+.PHONY: clean
+## Deletes all files created by Make
+clean: private_clean
+
+.PHONY: test
+## Runs the repository tests
+test: private_test
+
+# Includes
+include make/private.mk

bowerbird.mk
@@ -1,0 +1,2 @@
+_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+include $(_PATH)/src/bowerbird-help/bowerbird-help.mk

make/private.mk
@@ -1,0 +1,27 @@
+# Config
+.SUFFIXES:
+MAKEFLAGS += --no-builtin-rules
+MAKEFLAGS += --no-print-directory
+
+# Constants
+NAME ?= $(error ERROR: Undefined variable NAME)
+VERSION ?= $(error ERROR: Undefined variable VERSION)
+WORKDIR_ROOT ?= $(error ERROR: Undefined variable WORKDIR_ROOT)
+override WORKDIR_TEST = $(WORKDIR_ROOT)/test/$(NAME)/$(VERSION)
+
+# Includes
+include bowerbird.mk
+include test/bowerbird-help/test-help-message.mk
+
+ # Targets
+.PHONY: private_clean
+private_clean:
+	@echo "INFO: Cleaning directories:"
+	@$(if $(wildcard $(WORKDIR_ROOT)), rm -rfv $(WORKDIR_ROOT))
+	@$(if $(wildcard $(WORKDIR_TEST)), rm -rfv $(WORKDIR_TEST))
+	@echo "INFO: Cleaning complete."
+	@echo
+
+.PHONY: private_test
+private_test: \
+		test-help-message \

ic-designer/make-bowerbird-paths

Makefile
@@ -1,0 +1,22 @@
+# Constants
+.DEFAULT_GOAL := help
+
+NAME := boxerbird-paths
+VERSION := $(shell git describe --always --dirty --broken 2> /dev/null)
+WORKDIR_ROOT := $(CURDIR)/.make
+
+#Targets
+.PHONY: check
+## Runs the repository tests
+check: private_test
+
+.PHONY: clean
+## Deletes all files created by Make
+clean: private_clean
+
+.PHONY: test
+## Runs the repository tests
+test: private_test
+
+# Includes
+include make/private.mk

bowerbird.mk
@@ -1,0 +1,2 @@
+_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+include $(_PATH)/src/bowerbird-paths/bowerbird-paths.mk

make/private.mk
@@ -1,0 +1,27 @@
+# Config
+.SUFFIXES:
+MAKEFLAGS += --no-builtin-rules
+MAKEFLAGS += --no-print-directory
+
+# Constants
+NAME ?= $(error ERROR: Undefined variable NAME)
+VERSION ?= $(error ERROR: Undefined variable VERSION)
+WORKDIR_ROOT ?= $(error ERROR: Undefined variable WORKDIR_ROOT)
+override WORKDIR_TEST = $(WORKDIR_ROOT)/test/$(NAME)/$(VERSION)
+
+# Includes
+include bowerbird.mk
+include test/bowerbird-paths/test-directory-target.mk
+
+ # Targets
+.PHONY: private_clean
+private_clean:
+	@echo "INFO: Cleaning directories:"
+	@$(if $(wildcard $(WORKDIR_ROOT)), rm -rfv $(WORKDIR_ROOT))
+	@$(if $(wildcard $(WORKDIR_TEST)), rm -rfv $(WORKDIR_TEST))
+	@echo "INFO: Cleaning complete."
+	@echo
+
+.PHONY: private_test
+private_test: \
+		test-directory-target \